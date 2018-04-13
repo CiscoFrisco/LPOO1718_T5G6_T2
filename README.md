4c191215f5387b9c2c018f807cc5d3e672499eda
Unknown <pedromiguel.fernandes17@gmail.com>
2018-04-13 20:47:19 +0100

    updated project files

--- README.md
+++
@@ -1,2 +0,0 @@
-# LPOO1718_T5G6_T2
-Second project for FEUP MIEIC's curricular unit LPOO: AirHockey on Android.
---
+++ core/src/com/ciscominas/airhockeymania/MainMenu.java
@@ -0,0 +1,24 @@
+package com.ciscominas.airhockeymania;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MainMenu extends ScreenAdapter {
+
+    private final Game myGame;
+    private SpriteBatch spriteBatch;
+
+    public MainMenu(Game g)
+    {
+        myGame = g;
+    }
+
+    public void render(float delta)
+    {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    }
+}
