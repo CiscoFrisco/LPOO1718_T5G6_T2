a49a2f3f97f75643b6785c78708a0caa49779663
Francisco Filipe <francilipe.7@gmail.com>
2018-04-19 14:40:18 +0100

    cenas

---
+++ desktop/src/com/ciscominas/airhockeymania/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ciscominas.airhockeymania.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ciscominas.airhockeymania.AirHockeyMania;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AirHockeyMania(), config);
+	}
+}
